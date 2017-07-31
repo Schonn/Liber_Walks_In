4c2232b823cac39549c4a08e15b6422079c2814b
Schonn <hirs0054@flinders.edu.au>
2017-07-31 12:09:43 +0930

    Add files via upload
    Here is a skeleton structure for our addon!

---
+++ bodyscan_LSAT/__init__.py
@@ -0,0 +1,50 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+#addon info read by Blender
+bl_info = {
+    "name": "Lymphoedema 3D Scan Analysis Toolset",
+    "author": "Ivan Prga, Annette Nguyen, Jaspreet Kaur., Michael Stagg, Maxwell Heaysman, Schonn-Pierre Hirst",
+    "version": (1, 0, 0),
+    "blender": (2, 7, 4),
+    "description": "A toolset for importing, aligning and analysing 3D scans of lymphoedema patients.",
+    "category": "Mesh"
+    }
+
+#import blender python IDE libraries
+import bpy
+#use the following format to import other .py files from the local folder to make use of the contained classes/definitions
+#from . import (
+#        import_PLY_3DScan,
+#        register_landmark,
+#	 align_scans,
+#	 extract_volume,
+#	 generate_heatmap
+#        )
+
+#this function is called when the addon is loaded into Blender
+def register():
+    print("LSAT loaded")
+#this function is called when the addon is unloaded from Blender 
+def unregister():
+    print("LSAT unloaded")
+
+#for the purpose of testing, the following lines will allow the addon to be registered 
+#when this script is run in the Blender python IDE, without having to register the addon in 
+if __name__ == '__main__':
+    register()
